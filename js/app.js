23c0dcb42bafbe05a3f445ce6018058412fd2179
DaniGM17 <daniagm75@gmail.com>
2022-09-28 19:50:48 -0500

    Leer los datos del curso que seleccionamos

--- js/app.js
+++ js/app.js
@@ -18,6 +18,21 @@ function agregarCurso(e){
     e.preventDefault();
 
     if(e.target.classList.contains('agregar-carrito')){
-        console.log('Agregando al carrito')
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leeDatosCurso(cursoSeleccionado);
     }
+}
+//lee el contenido del html al dar click y extraer inf
+function leeDatosCurso(curso){
+    console.log(curso);
+
+    //crear objeto con el contenido del curso actual
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+    console.log(infoCurso);
 }
